8ae187905c2d20ee19be51ecc191a53dc846ca34
raingrain <2711835261@qq.com>
2022-10-18 23:18:40 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import 'antd/dist/antd.css';
 import "./index.scss";
 
-
-import App from "./App.jsx";
+import {App} from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App/>
