dcd77fbc92af418e7e3f431a74266e7e1971f1df
raingrain <2711835261@qq.com>
2022-12-09 09:43:45 +0800

    创建即将被删除的文本标注工具

---
+++ src/component/OperationPanel/OperationPanel.tsx
@@ -0,0 +1,18 @@
+//
+// import {TextToolbar} from "./TextToolbar";
+//
+// import {Collapse} from "antd";
+//
+// const {Panel} = Collapse;
+//
+// function OperationPanel() {
+//     return (
+//         <Collapse>
+//             <Panel header="TextToolbar" key="5">
+//                 <TextToolbar />
+//             </Panel>
+//         </Collapse>
+//     );
+// }
+//
+// export {OperationPanel};
