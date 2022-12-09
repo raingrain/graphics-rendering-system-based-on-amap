1891cfe7dcd560b4ebf2a1a6988c8dd7e4249666
raingrain <2711835261@qq.com>
2022-12-09 09:46:49 +0800

    Create index.tsx

---
+++ src/component/MapToolbar/index.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import {MapFeaturesTool} from "./MapFeaturesTool";
+import {MapLayerTool} from "./MapLayerTool";
+import {MapStyleTool} from "./MapStyleTool";
+import {PointTool} from "./PointTool";
+import {ClearTool} from "./ClearTool";
+import {PolylineTool} from "./PolylineTool";
+import {PolygonTool} from "./PolygonTool";
+
+export const MapToolbar = () => {
+    return (
+        <div style={{
+            display: "flex",
+            position: "absolute",
+            top: "1rem",
+            left: "600px"
+        }}>
+            <ClearTool/>
+            <PolygonTool/>
+            <PolylineTool/>
+            <PointTool />
+            <MapFeaturesTool />
+            <MapLayerTool />
+            <MapStyleTool />
+        </div>
+    );
+};
+
+export const ControlButton = styled.div`
+    height: 2rem;
+    width: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-left: 1px solid #e0e0e0;
+    background: #fff;
+    cursor: pointer;
+    
+    &:hover{
+        background-color: #d9d9d9;
+    }
+`;
