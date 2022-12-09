8a25f15bfd169c42057d445367d6c7fe34ae500c
raingrain <2711835261@qq.com>
2022-12-09 09:43:07 +0800

    创建底部信息栏

---
+++ src/component/BottomInfoPanel/index.tsx
@@ -0,0 +1,31 @@
+import {observer} from "mobx-react-lite";
+import {useStore} from "../../store";
+import styled from "styled-components";
+
+export const BottomInfoPanel = observer(() => {
+
+    const {mapInfos} = useStore();
+
+    return (
+        <Panel>
+            <div>zoom: {mapInfos.zoom}</div>
+            <div>mouseLngLat: {mapInfos.mouseLngLat}</div>
+            <div>mapCenter: {mapInfos.centerPoint}</div>
+        </Panel>
+    );
+});
+
+const Panel = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 250px;
+    height: 30px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    div {
+        margin-right: 2rem;
+    }
+`
+
