6b2cbfcb539aabf80a943711f7242638764f0431
raingrain <2711835261@qq.com>
2022-12-23 22:49:29 +0800

    移除一些多余的东西

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router";
+import {MapPage} from "./pages/MapPage";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<MapPage />);
--- src/router/index.tsx
+++
@@ -1,9 +0,0 @@
-import {createBrowserRouter} from "react-router-dom";
-import {MapPage} from "../pages/MapPage";
-
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MapPage />
-    }
-]);
